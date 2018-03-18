912edcccb408eeea04a74cd2d178828b413fbef7
azeelleeza <leezaluncheon@gmail.com>
2018-03-18 17:19:43 -0400

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,34 @@
+
 class BankAccount
+
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(x)
+    @balance= @balance + x
+
+  end
+
+  def display_balance
+    if @balance==@balance.round(2)
+      "Your balance is $#{@balance}."
+    else
+      "Your balance is $#{sprintf( "%0.02f", @balance)}."
+    end
+  end
+
+  def valid?
+    @status == "open" && @balance > 0
+  end
+
+  def close_account
+    @status = "closed"
+  end
 
 end
