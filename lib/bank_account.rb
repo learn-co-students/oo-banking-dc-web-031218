56d2f48f7d6d9930486627ad3e36b604fce991f9
rpyoungdale <youngdale95@gmail.com>
2018-03-19 10:47:50 -0400

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,35 @@
-class BankAccount
+require_relative "./transfer.rb"
 
+class BankAccount
+attr_reader :name
+attr_accessor :balance, :status
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(amount)
+    @balance+=amount
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    if @balance > 0 && @status == "open"
+      true
+    else
+      false
+    end
+  end
+
+  def close_account
+    self.status = "closed"
+  end
 end
+
+# An instance of the BackAccount class should be able to transfer
+# money to another instance through a Transer class.
